2c5eaf84ee8cc44ca4f96cbcb49fc61549507ea3
Vaishali Chandane <vaishalichandane543@gmail.com>
2026-05-18 10:37:02 +0530

    fixed razorpay popup

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,6 @@ const cors = require("cors");
 const bcrypt = require("bcryptjs");
 const Razorpay = require("razorpay");
 const authRoutes = require("./routes/auth");
-
 const User = require("./models/User");
 
 const app = express();
@@ -14,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/auth", authRoutes);
 app.use(express.json());
-app.use("/auth", require("./routes/auth"));
+
 
 /* =========================
    ✅ MONGODB CONNECTION
